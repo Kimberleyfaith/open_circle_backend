6a89bcd63d19c34a586ff9d7a588413aa2d28834
davidjohnson1562 <davidjohnson1562@penguin>
2021-05-18 12:02:01 +1000

    updated cors file to fix bug

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,18 +1,10 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3000'
+    origins 'http://localhost:3000', 'https://faithsylvia1.github.io/open_circle_frontend/'
 
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
       credentials: true
-  end
-  allow do
-    origins 'https://faithsylvia1.github.io/open_circle_frontend/'
-
-    resource '*',
-      headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head],
-      credentials: true
-  end
+  end 
 end
