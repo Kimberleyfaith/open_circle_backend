67e529aa51a19f700da0e06ae7873b451161de11
Kimberley Osborn <kimberleyfaith@Kimberleys-MacBook-Pro.local>
2021-05-18 11:44:41 +1000

    Update to the comments and post controllers

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,4 +13,58 @@ class PostsController < ApplicationController
       }
     end
   end
+
+  def show
+    @post = Post.find(params[:id])
+    if @post
+      render json: {
+        post: @post
+      }
+    else
+      render json: {
+        status: 500,
+        errors: ['post not found']
+      }
+    end
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      render json: {
+        status: :created,
+        post: @post
+      }
+    else
+      render json: {
+        status: 500,
+        errors: ['error: post not created']
+      }
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @post.update(post_params)
+        format.html { redirect_to @post, notice: "Post was successfully updated." }
+        format.json { render :show, status: :ok, location: @post }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @post.errors, status: :unprocessable_entity }
+      end
+    end
+
+    def destroy
+      @post.destroy
+      respond_to do |format|
+        format.html { redirect_to secrets_url, notice: "Post was successfully deleted." }
+        format.json { head :no_content }
+      end
+    end
+
+  private
+  def post_params
+    params.require(:post).permit(:image, :content, :likes)
+  end
+end
 end
