c04e0a6b35faa4133df44c6434638ebd280af995
davidjohnson1562 <davidjohnson1562@penguin>
2021-05-19 15:24:18 +1000

    updated session_store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env === 'production'
-  Rails.application.config.session_store :cookie_store, key: '_open_circle', domain: 'https://faithsylvia1.github.io/open_circle_frontend/'
+  Rails.application.config.session_store :cookie_store, key: '_open_circle', domain: 'faithsylvia1.github.io'
 else
   Rails.application.config.session_store :cookie_store, key: '_open_circle'
 end
