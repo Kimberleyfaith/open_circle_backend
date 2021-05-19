14c90a2b2bb3c1be62d8f74b2e62c2db16424235
davidjohnson1562 <davidjohnson1562@penguin>
2021-05-19 16:53:13 +1000

    updated initializers

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env === 'production'
-  Rails.application.config.session_store :cookie_store, key: '_open_circle', domain: :all
+  Rails.application.config.session_store :cookie_store, key: '_open_circle', domain: :all, secure: true
 else
   Rails.application.config.session_store :cookie_store, key: '_open_circle'
 end
