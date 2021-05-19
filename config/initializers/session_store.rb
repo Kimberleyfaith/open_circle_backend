13e540908e3d3f291360275c2449df2405663c2e
Kimberley Osborn <kimberleyfaith@Kimberleys-MacBook-Pro.local>
2021-05-19 15:39:35 +1000

    change the domain to all

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env === 'production'
-  Rails.application.config.session_store :cookie_store, key: '_open_circle', domain: 'faithsylvia1.github.io'
+  Rails.application.config.session_store :cookie_store, key: '_open_circle', domain: :all
 else
   Rails.application.config.session_store :cookie_store, key: '_open_circle'
 end
