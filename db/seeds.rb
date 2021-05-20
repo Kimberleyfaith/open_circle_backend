0536c7cf5e3bedd8ac88985f83864d681afecbea
Kimberley Osborn <kimberleyfaith@Kimberleys-MacBook-Pro.local>
2021-05-20 10:13:51 +1000

    update seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,19 @@
 User.destroy_all
-u1 = User.create :name => "Joel Turnbull", :email => "joel@ga.co", :password => "chicken", :profile_image => "https://i.pinimg.com/236x/1b/f1/ff/1bf1ff9e52d42ee99d679afdf7baf202--funny-face-pics-funny-baby-faces.jpg", :cover_image => "https://www.shell.com/energy-and-innovation/the-energy-future/scenarios/shell-scenario-sky/_jcr_content/pagePromo/image.img.960.jpeg/1548184031017/clear-blue-sky.jpeg", :bio => "Smile while you still have teeth.", :birthday => "1988-01-01", :location => "Sydney"
+u1 = User.create :name => "Joel Turnbull", :email => "joel@ga.co", :password => "chicken", :profile_image => "https://i.pinimg.com/236x/1b/f1/ff/1bf1ff9e52d42ee99d679afdf7baf202--funny-face-pics-funny-baby-faces.jpg", :cover_image => "https://www.shell.com/energy-and-innovation/the-energy-future/scenarios/shell-scenario-sky/_jcr_content/pagePromo/image.img.960.jpeg/1548184031017/clear-blue-sky.jpeg", :bio => "Smile while you still have teeth.", :birthday => "1982-07-15", :location => "Sydney"
 u2 = User.create :name => "Mai Nguyen", :email => "mai@ga.co", :password => "chicken", :profile_image => "https://i.pinimg.com/originals/29/3f/f8/293ff8faf8eccb20f7f44636fd82dba3.jpg", :cover_image => "https://resilientblog.co/wp-content/uploads/2019/07/sky-quotes.jpg", :bio => "Everyone is normal until you add them as your Facebook friend", :birthday => "1990-12-01", :location => "London"
 u3 = User.create :name => "Faith Sylvia", :email => "faith@ga.co", :password => "chicken", :profile_image => "https://i.pinimg.com/474x/98/bd/8b/98bd8beef5548efcfc810d8f4341fea9.jpg", :cover_image => "https://i.pinimg.com/originals/6b/c3/19/6bc319faa3cbbcfc5d98a740a908aaa7.jpg", :bio => "Lorem ipsum dolor sit amet, consectetur adipisicing elit.", :birthday => "1993-01-20", :location => "New Hampshire"
 u4 = User.create :name => "Chris Johnson", :email => "chris@ga.co", :password => "chicken", :profile_image => "https://i.pinimg.com/originals/af/ea/b3/afeab3e94a4627072c290221eb6f35c0.jpg", :cover_image => "https://wallpaperaccess.com/full/338765.jpg", :bio => "Dolcelatte halloumi mozzarella jarlsberg pepper jack say cheese babybel who moved my cheese. Gouda manchego.", :birthday => "1990-09-25", :location => "Sydney"
 u5 = User.create :name => "David Johnson", :email => "david@ga.co", :password => "chicken", :profile_image => "https://i.pinimg.com/564x/e2/51/9e/e2519e62a7ee2a6504d7e389f2f6c14e.jpg", :cover_image => "https://c.tadst.com/gfx/750w/barcelona-morning-sky.jpg?1", :bio => "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", :birthday => "1992-12-10", :location => "Florida"
 u6 = User.create :name => "Kimberley Osborn", :email => "kimberley@ga.co", :password => "chicken", :profile_image => "https://www.momtastic.com/assets/uploads/gallery/funny-faces-made-by-epically-cute-kids/funny-faces-made-by-epically-cute-kids-on-itsmomtastic-by-letmestart-featuring-fruit-cup-via-sophia-macheras-640x853.jpg", :cover_image => "https://toppng.com/uploads/preview/sunset-sky-pink-trees-sun-1157033920843x8eskgug.jpg", :bio => "Chalk and cheese queso taleggio. Hard cheese say cheese monterey jack parmesan halloumi hard cheese pepper jack cottage cheese.", :birthday => "1988-02-08", :location => "Johannesburg"
-
+u7 = User.create :name => "Audrey Patricia", :email => "audrey@ga.co", :password => "chicken", :profile_image => "https://www.rd.com/wp-content/uploads/2018/05/shutterstock_464300804.jpg", :cover_image => "", :bio => "Brie cheese triangles bavarian bergkase. Goat cream cheese cream cheese roquefort lancashire jarlsberg halloumi taleggio.", :birthday => "1988-02-08", :location => "Perth"
+u8 = User.create :name => "Bhavya Govind", :email => "bhavya@ga.co", :password => "chicken", :profile_image => "https://i.ytimg.com/vi/JNQU-4YEnm4/sddefault.jpg", :cover_image => "", :bio => "Paneer cheese strings caerphilly the big cheese pepper jack stinking bishop fromage mozzarella. Emmental bavarian bergkase bavarian bergkase taleggio dolcelatte halloumi hard cheese emmental.", :birthday => "1987-05-08", :location => "Melbourne"
+u9 = User.create :name => "David Ye", :email => "davidye@ga.co", :password => "chicken", :profile_image => "https://i.ytimg.com/vi/lfzJ3olqiug/maxresdefault.jpg", :cover_image => "", :bio => "Red leicester cheese triangles cheddar blue castello cheese and wine stilton smelly cheese when the cheese comes out everybody's happy.", :birthday => "1994-11-08", :location => "Syndey"
+u10 = User.create :name => "Katie Lugton", :email => "katie@ga.co", :password => "chicken", :profile_image => "", :cover_image => "", :bio => "Brie pepper jack cheese and biscuits chalk and cheese cheese on toast chalk and cheese the big cheese camembert de normandie.", :birthday => "1992-02-22", :location => "Liverpool"
+u11 = User.create :name => "Mauritz Erick", :email => "mauritz@ga.co", :password => "chicken", :profile_image => "", :cover_image => "", :bio => "Cheese on toast melted cheese port-salut camembert de normandie blue castello cut the cheese babybel cheesy feet. ", :birthday => "1988-02-08", :location => "Manchester"
+u12 = User.create :name => "Paul Calnan", :email => "paul@ga.co", :password => "chicken", :profile_image => "https://i.pinimg.com/originals/a8/06/f1/a806f18278f47647cd96108ae861abba.jpg", :cover_image => "", :bio => "Taleggio stilton cottage cheese say cheese cheese on toast jarlsberg feta st. agur blue cheese.", :birthday => "1971-01-29", :location => "Hobart"
+u13 = User.create :name => "Priyanka Patel", :email => "priyanka@ga.co", :password => "chicken", :profile_image => "", :cover_image => "", :bio => "Cheddar cheese and wine mascarpone. Manchego say cheese airedale parmesan hard cheese dolcelatte chalk and cheese bavarian bergkase.", :birthday => "1980-01-08", :location => "Adelaide"
+u14 = User.create :name => "Qiong Dai", :email => "nelly@ga.co", :password => "chicken", :profile_image => "", :cover_image => "", :bio => "Cream cheese edam queso lancashire cheese triangles stinking bishop edam fromage frais. Cheese strings boursin taleggio pecorino port-salut boursin mozzarella.", :birthday => "1991-11-12", :location => "London"
+u15 = User.create :name => "Sam Gelwyn", :email => "sam@ga.co", :password => "chicken", :profile_image => "https://i.pinimg.com/originals/b2/66/da/b266da7035ffb55760bb43157637456a.jpg", :cover_image => "", :bio => "Brie jarlsberg macaroni cheese paneer cheese on toast cottage cheese mozzarella cheese triangles.", :birthday => "1980-04-12", :location => "Essex"
 
 Post.destroy_all
 p1 = Post.create :images => ["https://i.pinimg.com/236x/25/67/b4/2567b4e6686326e5b39eb5e1ed149033--puppy-play-puppy-love.jpg"], :content => "Meet Baxter"
@@ -36,6 +44,7 @@ o2 = Circle.create :name => "Colleagues", :image => "https://positiveroutines.co
 o3 = Circle.create :name => "Family", :image => "https://content.thriveglobal.com/wp-content/uploads/2019/11/family-.jpg"
 o4 = Circle.create :name => "Fantasy Football", :image => "https://miro.medium.com/max/812/1*t3Yer2tVwRZjfFqXy94jMg.png"
 puts "#{ Circle.count } circles created."
+o5 = Circle.create :name => "GA", :image => "https://atlantaservicejam.files.wordpress.com/2016/02/general-assembly-logo-600.gif"
 
 Chatroom.destroy_all
 r1 = Chatroom.create :title => "The Four Lokos"
@@ -45,26 +54,49 @@ puts "#{ Chatroom.count } chatrooms created."
 Like.destroy_all
 l1 = Like.create
 l2 = Like.create
+l3 = Like.create
+l4 = Like.create
+l5 = Like.create
+l6 = Like.create
+l7 = Like.create
+l8 = Like.create
+l9 = Like.create
+l10 = Like.create
+l11 = Like.create
+l12 = Like.create
 
 puts "Posts and Users"
 u1.posts << p1
 u2.posts << p2
 
 puts "Posts and Circles"
-o1.posts << p1 << p3 << p5
-o2.posts << p2 << p4 << p6
+o1.posts << p1 << p2
+o2.posts << p5
+o3.posts << p3
+o4.posts << p4
+o5.posts << p6
 
 puts "Messages and Users"
 u1.messages << m1
 u2.messages << m2
 
 puts "Circles and Users"
-u1.circles << o1 << o3
-u2.circles << o2 << o4
-u3.circles << o1 << o2
-u4.circles << o2
-u5.circles << o3 << o4
-u6.circles << o4
+u1.circles << o1 << o3 << o5
+u2.circles << o2 << o4 << o5
+u3.circles << o1 << o2 << o5
+u4.circles << o2 << o5
+u5.circles << o3 << o4 << o5
+u6.circles << o4 << o2 << o5
+u7.circles << o5
+u8.circles << o5 << o2
+u9.circles << o5 << o1
+u10.circles << o5 << o3
+u11.circles << o5
+u12.circles << o5 << o3
+u13.circles << o5
+u14.circles << o5 << o3
+u15.circles << o5 << o1
+
 
 puts "Messages and Chatrooms"
 r1.messages << m1
