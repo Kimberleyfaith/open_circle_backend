104a05991f55bae47b518da3f78d0a14d35f1686
Kimberley Osborn <kimberleyfaith@Kimberleys-MacBook-Pro.local>
2021-05-20 10:50:18 +1000

    Update seed data v3

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,15 +5,15 @@ u3 = User.create :name => "Faith Sylvia", :email => "faith@ga.co", :password => 
 u4 = User.create :name => "Chris Johnson", :email => "chris@ga.co", :password => "chicken", :profile_image => "https://i.pinimg.com/originals/af/ea/b3/afeab3e94a4627072c290221eb6f35c0.jpg", :cover_image => "https://wallpaperaccess.com/full/338765.jpg", :bio => "Dolcelatte halloumi mozzarella jarlsberg pepper jack say cheese babybel who moved my cheese. Gouda manchego.", :birthday => "1990-09-25", :location => "Sydney"
 u5 = User.create :name => "David Johnson", :email => "david@ga.co", :password => "chicken", :profile_image => "https://i.pinimg.com/564x/e2/51/9e/e2519e62a7ee2a6504d7e389f2f6c14e.jpg", :cover_image => "https://c.tadst.com/gfx/750w/barcelona-morning-sky.jpg?1", :bio => "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", :birthday => "1992-12-10", :location => "Florida"
 u6 = User.create :name => "Kimberley Osborn", :email => "kimberley@ga.co", :password => "chicken", :profile_image => "https://www.momtastic.com/assets/uploads/gallery/funny-faces-made-by-epically-cute-kids/funny-faces-made-by-epically-cute-kids-on-itsmomtastic-by-letmestart-featuring-fruit-cup-via-sophia-macheras-640x853.jpg", :cover_image => "https://toppng.com/uploads/preview/sunset-sky-pink-trees-sun-1157033920843x8eskgug.jpg", :bio => "Chalk and cheese queso taleggio. Hard cheese say cheese monterey jack parmesan halloumi hard cheese pepper jack cottage cheese.", :birthday => "1988-02-08", :location => "Johannesburg"
-u7 = User.create :name => "Audrey Patricia", :email => "audrey@ga.co", :password => "chicken", :profile_image => "https://www.rd.com/wp-content/uploads/2018/05/shutterstock_464300804.jpg", :cover_image => "", :bio => "Brie cheese triangles bavarian bergkase. Goat cream cheese cream cheese roquefort lancashire jarlsberg halloumi taleggio.", :birthday => "1988-02-08", :location => "Perth"
-u8 = User.create :name => "Bhavya Govind", :email => "bhavya@ga.co", :password => "chicken", :profile_image => "https://i.ytimg.com/vi/JNQU-4YEnm4/sddefault.jpg", :cover_image => "", :bio => "Paneer cheese strings caerphilly the big cheese pepper jack stinking bishop fromage mozzarella. Emmental bavarian bergkase bavarian bergkase taleggio dolcelatte halloumi hard cheese emmental.", :birthday => "1987-05-08", :location => "Melbourne"
-u9 = User.create :name => "David Ye", :email => "davidye@ga.co", :password => "chicken", :profile_image => "https://i.ytimg.com/vi/lfzJ3olqiug/maxresdefault.jpg", :cover_image => "", :bio => "Red leicester cheese triangles cheddar blue castello cheese and wine stilton smelly cheese when the cheese comes out everybody's happy.", :birthday => "1994-11-08", :location => "Syndey"
-u10 = User.create :name => "Katie Lugton", :email => "katie@ga.co", :password => "chicken", :profile_image => "", :cover_image => "", :bio => "Brie pepper jack cheese and biscuits chalk and cheese cheese on toast chalk and cheese the big cheese camembert de normandie.", :birthday => "1992-02-22", :location => "Liverpool"
-u11 = User.create :name => "Mauritz Erick", :email => "mauritz@ga.co", :password => "chicken", :profile_image => "", :cover_image => "", :bio => "Cheese on toast melted cheese port-salut camembert de normandie blue castello cut the cheese babybel cheesy feet. ", :birthday => "1988-02-08", :location => "Manchester"
-u12 = User.create :name => "Paul Calnan", :email => "paul@ga.co", :password => "chicken", :profile_image => "https://i.pinimg.com/originals/a8/06/f1/a806f18278f47647cd96108ae861abba.jpg", :cover_image => "", :bio => "Taleggio stilton cottage cheese say cheese cheese on toast jarlsberg feta st. agur blue cheese.", :birthday => "1971-01-29", :location => "Hobart"
-u13 = User.create :name => "Priyanka Patel", :email => "priyanka@ga.co", :password => "chicken", :profile_image => "", :cover_image => "", :bio => "Cheddar cheese and wine mascarpone. Manchego say cheese airedale parmesan hard cheese dolcelatte chalk and cheese bavarian bergkase.", :birthday => "1980-01-08", :location => "Adelaide"
-u14 = User.create :name => "Qiong Dai", :email => "nelly@ga.co", :password => "chicken", :profile_image => "", :cover_image => "", :bio => "Cream cheese edam queso lancashire cheese triangles stinking bishop edam fromage frais. Cheese strings boursin taleggio pecorino port-salut boursin mozzarella.", :birthday => "1991-11-12", :location => "London"
-u15 = User.create :name => "Sam Gelwyn", :email => "sam@ga.co", :password => "chicken", :profile_image => "https://i.pinimg.com/originals/b2/66/da/b266da7035ffb55760bb43157637456a.jpg", :cover_image => "", :bio => "Brie jarlsberg macaroni cheese paneer cheese on toast cottage cheese mozzarella cheese triangles.", :birthday => "1980-04-12", :location => "Essex"
+u7 = User.create :name => "Audrey Patricia", :email => "audrey@ga.co", :password => "chicken", :profile_image => "https://www.rd.com/wp-content/uploads/2018/05/shutterstock_464300804.jpg", :cover_image => "https://www.redcliffetravel.com.au/images/_large/best_sunrises_and_sunsets_in_queensland.jpg", :bio => "Brie cheese triangles bavarian bergkase. Goat cream cheese cream cheese roquefort lancashire jarlsberg halloumi taleggio.", :birthday => "1988-02-08", :location => "Perth"
+u8 = User.create :name => "Bhavya Govind", :email => "bhavya@ga.co", :password => "chicken", :profile_image => "https://i.ytimg.com/vi/JNQU-4YEnm4/sddefault.jpg", :cover_image => "https://i.pinimg.com/originals/6a/96/73/6a967367de0c43bfdf30808eb693547a.jpg", :bio => "Paneer cheese strings caerphilly the big cheese pepper jack stinking bishop fromage mozzarella. Emmental bavarian bergkase bavarian bergkase taleggio dolcelatte halloumi hard cheese emmental.", :birthday => "1987-05-08", :location => "Melbourne"
+u9 = User.create :name => "David Ye", :email => "davidye@ga.co", :password => "chicken", :profile_image => "https://i.ytimg.com/vi/lfzJ3olqiug/maxresdefault.jpg", :cover_image => "https://cdn.vox-cdn.com/thumbor/Al48-pEnyIn2rlgKX7MIHNmlE68=/0x0:5563x3709/1200x800/filters:focal(2302x1311:3192x2201)/cdn.vox-cdn.com/uploads/chorus_image/image/65752607/1048232144.jpg.0.jpg", :bio => "Red leicester cheese triangles cheddar blue castello cheese and wine stilton smelly cheese when the cheese comes out everybody's happy.", :birthday => "1994-11-08", :location => "Syndey"
+u10 = User.create :name => "Katie Lugton", :email => "katie@ga.co", :password => "chicken", :profile_image => "https://static8.depositphotos.com/1000387/876/i/600/depositphotos_8767134-stock-photo-amazed-or-surprised-child-boy.jpg", :cover_image => "https://ntnow.com.au/photos/1/128927_Sunset_over_Darwin_Harbour_credit_Tourism_NT_Jules_Ingall.jpg", :bio => "Brie pepper jack cheese and biscuits chalk and cheese cheese on toast chalk and cheese the big cheese camembert de normandie.", :birthday => "1992-02-22", :location => "Liverpool"
+u11 = User.create :name => "Mauritz Erick", :email => "mauritz@ga.co", :password => "chicken", :profile_image => "https://i.pinimg.com/236x/bf/bc/96/bfbc96241fcdb1ea361d724a61fee990--facial-expressions-surprise.jpg", :cover_image => "https://glamadelaide.com.au/wp-content/uploads/2019/12/s-sunset-beach.jpg", :bio => "Cheese on toast melted cheese port-salut camembert de normandie blue castello cut the cheese babybel cheesy feet. ", :birthday => "1988-02-08", :location => "Manchester"
+u12 = User.create :name => "Paul Calnan", :email => "paul@ga.co", :password => "chicken", :profile_image => "https://i.pinimg.com/originals/a8/06/f1/a806f18278f47647cd96108ae861abba.jpg", :cover_image => "https://particle.azureedge.net/app/uploads/2020/06/photo_2020-06-19_10-32-48-e1592547249260.jpg", :bio => "Taleggio stilton cottage cheese say cheese cheese on toast jarlsberg feta st. agur blue cheese.", :birthday => "1971-01-29", :location => "Hobart"
+u13 = User.create :name => "Priyanka Patel", :email => "priyanka@ga.co", :password => "chicken", :profile_image => "https://www.askideas.com/wp-content/uploads/2016/11/Funny-Shocked-Baby-Face-Picture.jpg", :cover_image => "https://media.tacdn.com/media/attractions-splice-spp-674x446/0a/9d/29/44.jpg", :bio => "Cheddar cheese and wine mascarpone. Manchego say cheese airedale parmesan hard cheese dolcelatte chalk and cheese bavarian bergkase.", :birthday => "1980-01-08", :location => "Adelaide"
+u14 = User.create :name => "Qiong Dai", :email => "nelly@ga.co", :password => "chicken", :profile_image => "https://cdn2.momjunction.com/wp-content/uploads/2015/03/100-Most-Popular-And-Funny-Baby-Names-Of-2018-Revealed.jpg", :cover_image => "https://upload.wikimedia.org/wikipedia/commons/a/a4/Anatomy_of_a_Sunset-2.jpg", :bio => "Cream cheese edam queso lancashire cheese triangles stinking bishop edam fromage frais. Cheese strings boursin taleggio pecorino port-salut boursin mozzarella.", :birthday => "1991-11-12", :location => "London"
+u15 = User.create :name => "Sam Gelwyn", :email => "sam@ga.co", :password => "chicken", :profile_image => "https://i.pinimg.com/originals/b2/66/da/b266da7035ffb55760bb43157637456a.jpg", :cover_image => "https://c.tadst.com/gfx/600x337/barcelona-morning-sky.jpg?1", :bio => "Brie jarlsberg macaroni cheese paneer cheese on toast cottage cheese mozzarella cheese triangles.", :birthday => "1980-04-12", :location => "Essex"
 
 Post.destroy_all
 p1 = Post.create :images => ["https://i.pinimg.com/236x/25/67/b4/2567b4e6686326e5b39eb5e1ed149033--puppy-play-puppy-love.jpg"], :content => "Meet Baxter"
@@ -26,6 +26,46 @@ puts "#{ Post.count } posts created."
 p5 = Post.create :content => "Congratulations on your engagement!"
 puts "#{ Post.count } posts created."
 p6 = Post.create :content => "Happy Birthday!"
+puts "#{ Post.count } posts created."
+p7 = Post.create :content => "Post 7"
+puts "#{ Post.count } posts created."
+p8 = Post.create :content => "Post 8"
+puts "#{ Post.count } posts created."
+p9 = Post.create :content => "Post 9"
+puts "#{ Post.count } posts created."
+p10 = Post.create :content => "Post 10"
+puts "#{ Post.count } posts created."
+p11 = Post.create :content => "Post 11"
+puts "#{ Post.count } posts created."
+p12 = Post.create :content => "Post 12"
+puts "#{ Post.count } posts created."
+p13 = Post.create :content => "Post 13"
+puts "#{ Post.count } posts created."
+p14 = Post.create :content => "Post 14"
+puts "#{ Post.count } posts created."
+p15 = Post.create :content => "Post 15"
+puts "#{ Post.count } posts created."
+p16 = Post.create :content => "Post 16"
+puts "#{ Post.count } posts created."
+p17 = Post.create :content => "Post 17"
+puts "#{ Post.count } posts created."
+p18 = Post.create :content => "Post 18"
+puts "#{ Post.count } posts created."
+p19 = Post.create :content => "Post 19"
+puts "#{ Post.count } posts created."
+p20 = Post.create :content => "Post 20"
+puts "#{ Post.count } posts created."
+p21 = Post.create :content => "Post 21"
+puts "#{ Post.count } posts created."
+p22 = Post.create :content => "Post 22"
+puts "#{ Post.count } posts created."
+p23 = Post.create :content => "Post 23"
+puts "#{ Post.count } posts created."
+p24 = Post.create :content => "Post 24"
+puts "#{ Post.count } posts created."
+p25 = Post.create :content => "Post 25"
+puts "#{ Post.count } posts created."
+p26 = Post.create :content => "Post 26"
 puts "#{ Post.count } posts created."
 
 Message.destroy_all
@@ -66,8 +106,23 @@ l11 = Like.create
 l12 = Like.create
 
 puts "Posts and Users"
-u1.posts << p1
-u2.posts << p2
+u1.posts << p1 << p2 << p3 << p4 << p5
+u2.posts << p6 << p19
+u3.posts << p7 << p20
+u4.posts << p8 << p21 << p22
+u5.posts << p9
+u6.posts << p10
+u7.posts << p11
+u8.posts << p12
+u9.posts << p13 << p23
+u10.posts << p14
+u11.posts << p15
+u12.posts << p15 << p24 << p25 << p26
+u12.posts << p16
+u14.posts << p17
+u15.posts << p18
+
+
 
 puts "Posts and Circles"
 o1.posts << p1 << p2
